8b9a99465c3daf1a87f02ba6630acc8ed55e4aea
AnGeLZhiL <angelina.zhilyakova.2002@mail.ru>
2022-11-19 12:09:20 +0300

    Навигация и фрагменты

--- app/src/main/java/com/example/wsapp/MainActivity.kt
+++ app/src/main/java/com/example/wsapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.wsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.wsapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+private lateinit var binding: ActivityMainBinding
+private lateinit var navController: NavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        setupWithNavController(binding.bottomNavigationView, navController)
     }
 }
